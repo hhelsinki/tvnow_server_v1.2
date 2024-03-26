c75542a31049da6161fdf51268032fa99089a093
hhelsinki <bongkotsaelo.cmtc@gmail.com>
2024-03-26 17:56:31 +0700

    update-api-refactor

---
+++ services/GenGiftcode.ts
@@ -0,0 +1,26 @@
+//@ts-ignore
+var crypto = require('crypto');
+// Create the generator:
+var randtoken = require('rand-token').generator({
+    chars: 'base32',
+    //@ts-ignore
+    source: crypto.randomBytes
+});
+
+//@ts-ignore
+const pools = require('../mysql/database.ts');
+
+// Generate a 16 character token:
+const token = randtoken.generate(16);
+
+function insertGiftcard() {
+    //@ts-ignore
+    pools.query('INSERT INTO giftcard (code) VALUES (?)', [token], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    })
+}
+
+insertGiftcard();
+
+console.log(token);
